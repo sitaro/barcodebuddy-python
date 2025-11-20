84f33f9f7f76a9755aec7cc6828b680a52bafcc3
Mathias Päzolt <mathias@mathops.de>
2025-11-20 09:10:32 +0100

    Bump version to 2.0.3

--- barcodebuddy/run.sh
+++ barcodebuddy/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "🚀 Starting Barcode Buddy (Python) v2.0.2"
+echo "🚀 Starting Barcode Buddy (Python) v2.0.3"
 echo "==========================================="
 
 # Show configuration
