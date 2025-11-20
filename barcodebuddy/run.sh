49d757f837997904584b52e8cc6f8ebab7d48c31
Mathias Päzolt <mathias@mathops.de>
2025-11-20 10:27:35 +0100

    Bump version to 2.2.0 - Multi-scanner support!

--- barcodebuddy/run.sh
+++ barcodebuddy/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "🚀 Starting Barcode Buddy (Python) v2.1.2"
+echo "🚀 Starting Barcode Buddy (Python) v2.2.0"
 echo "==========================================="
 
 # Show configuration
