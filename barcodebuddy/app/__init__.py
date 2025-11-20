6ce5d31b03bd1663c08cd066c1e6c7e7bf7d889a
Mathias Päzolt <mathias@mathops.de>
2025-11-20 09:23:25 +0100

    Bump version to 2.0.7

--- barcodebuddy/app/__init__.py
+++ barcodebuddy/app/__init__.py
@@ -1,2 +1,2 @@
 """Barcode Buddy Python Application."""
-__version__ = "2.0.6"
+__version__ = "2.0.7"
--- barcodebuddy/run.sh
+++ barcodebuddy/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "🚀 Starting Barcode Buddy (Python) v2.0.6"
+echo "🚀 Starting Barcode Buddy (Python) v2.0.7"
 echo "==========================================="
 
 # Show configuration
